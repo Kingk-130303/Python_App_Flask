288555f0d7937843b7ba94e93fe44812e9e24403
Kaival <mailkaivalshah@gmail.com>
2024-04-14 18:18:02 +0530

    use make_response for explicit response and add code to respond request at different routes

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
-from flask import Flask,jsonify
+from flask import Flask, make_response
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    # return {"message": "Hello, World!"}
-    return jsonify(message="Hello, World!")
+
+@app.route("/")
+def index():
+    return "hello world"
+
+@app.route("/no_content")
+def no_content():
+    """return 'no content found' with a status of 204
+
+    Returns:
+        string: no content found with 204 status code
+    """
+    return ({"message":"No content found"}, 204)
+
+@app.route("/exp")
+def index_explicit():
+    """return 'Hello World' message with a status code of 200
+
+    Returns:
+        string: Hello World
+        status code: 200
+    """
+    resp = make_response({"message":"Hello World"})
+    resp.status_code = 200
+    return resp
